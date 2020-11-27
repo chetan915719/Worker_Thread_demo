e5d743bed3cfc246a6df15b8a4f422026ebec737
chetan915719 <Chetantiwari661@gmail.com>
2020-11-27 12:49:57 +0530

    worker pool added

---
+++ simple/main.js
@@ -0,0 +1,20 @@
+const firstName = require("../data/first_name.json");
+const middleName = require("../data/middle_name.json");
+const lastName = require("../data/last_name.json");
+const fs = require('fs-extra');
+const {getRandomIndex} = require("../utils")
+
+const outputFile = '../output/data.txt';
+
+const limit = 1000;
+
+console.time();
+(async()=>{
+    for (let i = 0; i < limit; i++){
+        const data = [firstName, middleName, lastName].map(getRandomIndex).concat("\n").join(" ");
+        await fs.appendFile(outputFile,data);
+        if(i==limit-1){
+            console.timeEnd();
+        }
+    }
+})();
