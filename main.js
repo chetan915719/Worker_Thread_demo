04a6af96d3d8ce65fe272e5509ce89133603d28e
chetan915719 <Chetantiwari661@gmail.com>
2020-11-27 11:45:15 +0530

    comparision between simple and worker thread program

--- main.js
+++ main.js
@@ -1,6 +1,4 @@
 const { Worker } = require("worker_threads");
-const logUpdate = require("log-update");
-const fs = require('fs-extra');
 
 const limit = 1000000;
 const threads = 10;
@@ -9,16 +7,21 @@ const outputFile = './output/data.txt';
 
 let names = [...Array(threads)].fill(0);
 
-for (let i = 0; i < threads; i++) {
-    const port = new Worker(require.resolve("./worker.js"), {
-        workerData: {namesPerThread, outputFile}
+function runWorker(){
+    return new Promise((resolve, reject) => {
+        for (let i = 0; i < threads; i++) {
+            const port = new Worker(require.resolve("./worker.js"), {
+                workerData: { namesPerThread, outputFile, i }
+            });
+            port.on("message", resolve);
+            port.on("error", reject);
+            port.on("exit", (code) => {
+                if (code !== 0) {
+                    reject(new Error(`Worker stopped with exit code ${code}`));
+                }
+            });
+        }
     });
-    port.on("message", (data) => handleMessage(data,i));
-    port.on("error", (e) => console.log(e));
-    port.on("exit", (code) => console.log(code));
 }
 
-function handleMessage(_,index) {
-    names[index]++;
-    logUpdate(names.map((status,i) => `Thread ${i}: ${status}`).join("\n"));
-}
+runWorker();
